ece701afda24e59d4d25ce22a004ee56452f5f88
Jacob Sebastian Bass <bass@ou.edu>
2020-01-11 13:50:48 -0600

    initial

---
+++ MIS3013Demo/ImageHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MIS3013Demo
+{
+    public static class ImageHelper
+    {
+        public static int[][] LoadPixelValuesFromPath(string path, int height, int width)
+        {
+            var image = new Bitmap(path);
+            var resized = Resize(image, height, width);
+            return GetLuminanceValues(resized);
+        }
+
+        private static Bitmap Resize(Bitmap image, int desiredHeight, int desiredWidth)
+        {
+            var scale = Math.Min((float)desiredWidth / image.Width, (float)desiredHeight / image.Height);
+            var newHeight = (int)(image.Height * scale);
+            var newWidth = (int)(image.Width * scale);
+            return new Bitmap(image, new Size(newWidth, newHeight));
+        }
+
+        /// <summary>
+        /// Converts an image into a 2d array of luminance values (0-255)
+        /// </summary>
+        /// <param name="image">Image to process</param>
+        /// <returns></returns>
+        private static int[][] GetLuminanceValues(Bitmap image)
+        {
+            int[][] pixelValues = new int[image.Width][];
+            for (var x = 0; x < image.Width; x++)
+            {
+                pixelValues[x] = new int[image.Height];
+                for (var y = 0; y < image.Height; y++)
+                {
+                    var pixel = image.GetPixel(x, y);
+                    pixelValues[x][y] = (int)((pixel.R * 0.2126) + (pixel.G * 0.7152) + (pixel.B * 0.0722));
+                }
+            }
+
+            return pixelValues;
+        }
+    }
+}
---
+++ MIS3013Demo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+
+namespace MIS3013Demo
+{
+    class Program
+    {
+        const string exitString = "n";
+        const int HEIGHT = 24*4;
+        const int WIDTH = (int)(80 * 2.5) * 4;
+
+        static void Main(string[] args)
+        {
+            int[][] pixelValues = null;
+            while(true)
+            {
+                Console.WriteLine($"Enter a path to an image (or \"{exitString}\" to cancel):");
+                var file = Console.ReadLine();
+                if(file.ToLower() == exitString)
+                {
+                    break;
+                }
+
+                try
+                {
+                    pixelValues = ImageHelper.LoadPixelValuesFromPath(file, HEIGHT, WIDTH);
+                    Console.WriteLine("Loaded image");
+
+                    //Output to Console
+                    for (var y = 0; y < pixelValues[0].Length; y++)
+                    {
+                        for (var x = 0; x < pixelValues.Length; x++)
+                        {
+                            Console.Write(GetCharacterForValue(pixelValues[x][y]));
+                        }
+                        Console.Write("\n");
+                    }
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine($"Could not find image at: {file}");
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static char GetCharacterForValue(int value)
+        {
+            if(value < 25)
+            {
+                return ' ';
+            }
+            else if (value < 50)
+            {
+                return '.';
+            }
+            else if (value < 75)
+            {
+                return ':';
+            }
+            else if (value < 100)
+            {
+                return '-';
+            }
+            else if (value < 125)
+            {
+                return '=';
+            }
+            else if (value < 150)
+            {
+                return '+';
+            }
+            else if (value < 175)
+            {
+                return '*';
+            }
+            else if (value < 200)
+            {
+                return '#';
+            }
+            else if (value < 225)
+            {
+                return '%';
+            }
+            else
+            {
+                return '@';
+            }
+        }
+    }
+}
